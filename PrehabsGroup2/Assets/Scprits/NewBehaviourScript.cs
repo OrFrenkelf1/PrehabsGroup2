f60da43b76157a4caafaa22ca572842a588a5f2c
Leeshap98 <1998lee@gmail.com>
2023-06-15 18:39:46 +0300

    Added coroutine

--- PrehabsGroup2/Assets/Scprits/NewBehaviourScript.cs
+++ PrehabsGroup2/Assets/Scprits/NewBehaviourScript.cs
@@ -1,10 +1,86 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class NewBehaviourScript : MonoBehaviour
+//{
+
+//    [SerializeField] GameObject hat;
+//    [SerializeField] GameObject hatEars;
+//    [SerializeField] GameObject bunny;
+//    [SerializeField] GameObject amongUs;
+//    [SerializeField] GameObject poof;
+
+//    [SerializeField] Animator anim;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        print("start");
+//        StartCoroutine(Example());
+//    }
+
+//    IEnumerator Example()
+//    {
+//        print("IEnumerator");
+//        if (Input.GetKey(KeyCode.F))
+//        {
+//            First();
+//            print("first");
+//            anim.SetTrigger("Morph");
+
+//            yield return new WaitForSeconds(1);
+//            Second();
+//            print("second");
+//            anim.SetTrigger("Morph2");
+//        }
+//    }
+//    //private void Update()
+//    //{
+
+//    //    if (Input.GetKey(KeyCode.F))
+//    //    {
+//    //        First();
+//    //        anim.SetTrigger("Morph");
+
+//    //        Second();
+//    //        anim.SetTrigger("Morph2");
+//    //    }
+
+
+//    //    //if (Input.GetKeyUp(KeyCode.F))
+//    //    //{
+//    //    //    Second();
+//    //    //    anim.SetTrigger("Morph2");
+//    //    //}
+//    //}
+
+
+//    void First()
+//    {
+//        print("first");
+//        hat.SetActive(true);
+//        bunny.SetActive(true);
+//        poof.SetActive(true);
+//        hatEars.SetActive(false);
+//        amongUs.SetActive(false);
+//    }
+
+//    void Second()
+//    {
+//        print("second");
+//        bunny.SetActive(false);
+//        amongUs.SetActive(true);
+//    }
+
+//}
+
+
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
     [SerializeField] GameObject hat;
     [SerializeField] GameObject hatEars;
     [SerializeField] GameObject bunny;
@@ -13,33 +89,36 @@ public class NewBehaviourScript : MonoBehaviour
 
     [SerializeField] Animator anim;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        print("start");
-    }
+    private bool isCoroutineRunning = false;
 
     private void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKey(KeyCode.F) && !isCoroutineRunning)
         {
-            First();
-            anim.SetTrigger("Morph");
-        }
-
-
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            Second();
-            anim.SetTrigger("Morph");
+            StartCoroutine(Example());
         }
     }
 
+    IEnumerator Example()
+    {
+        isCoroutineRunning = true;
+
+        First();
+        anim.SetTrigger("Morph");
+
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+
+
+        Second();
+        anim.SetTrigger("Morph2");
+
+
+
+        isCoroutineRunning = false;
+    }
 
     void First()
     {
-        print("first");
         hat.SetActive(true);
         bunny.SetActive(true);
         poof.SetActive(true);
@@ -49,11 +128,8 @@ public class NewBehaviourScript : MonoBehaviour
 
     void Second()
     {
-        print("second");
         bunny.SetActive(false);
         amongUs.SetActive(true);
-
-
     }
-
 }
+
